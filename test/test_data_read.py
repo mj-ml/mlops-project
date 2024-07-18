efcdc10d3a3b82393a88e62aca3729be98f28ee6
Mikel <mii.jablonski@gmail.com>
2024-07-18 22:58:17 +0200

    tests

--- test/test_data_read.py
+++ test/test_data_read.py
@@ -1,4 +1,10 @@
-from src.read_data import read_load, read_weather, extract_load, extract_weather, create_dataframe
+from src.read_data import (
+    read_load,
+    read_weather,
+    extract_load,
+    extract_weather,
+    create_dataframe,
+)
 
 
 def test_load_data_read():
@@ -16,6 +22,7 @@ def test_extract_load():
     df_load = extract_load(df)
     assert df_load.empty is False
 
+
 def test_extract_weather():
     df = read_weather()
     df_weather = extract_weather(df)
@@ -27,5 +34,8 @@ def test_create_dataframe():
     df_weather = extract_weather(df)
     df = read_load()
     df_load = extract_load(df)
-    df_all = create_dataframe(df_weather, df_load)
+    df_all = create_dataframe(
+        df_weather,
+        df_load,
+    )
     assert df_all.empty is False
