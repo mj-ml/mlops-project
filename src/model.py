e29e1a1b68dda1ca7ecd810270f802c0c229abbc
Mikel <mii.jablonski@gmail.com>
2024-07-19 11:54:52 +0200

    running pipeline - pipeline local - docker compose

--- src/model.py
+++ src/model.py
@@ -3,7 +3,7 @@ from typing import Tuple
 import numpy as np
 import pandas as pd
 from sklearn.ensemble import RandomForestRegressor
-from sklearn.metrics import mean_squared_error
+from sklearn.metrics import mean_squared_error, mean_absolute_percentage_error
 from sklearn.model_selection import train_test_split
 
 import mlflow
@@ -51,7 +51,7 @@ def split_train_test(df: pd.DataFrame) -> Tuple[pd.DataFrame, pd.DataFrame]:
 
 def train_model(train_df: pd.DataFrame, test_df: pd.DataFrame) -> None:
     search_space = {
-        "max_depth": scope.int(hp.quniform("max_depth", 1, 10, 1)),
+        "max_depth": scope.int(hp.quniform("max_depth", 1, 10, 2)),
         "random_state": 42,
     }
     target_variable = "total load actual"
@@ -63,27 +63,24 @@ def train_model(train_df: pd.DataFrame, test_df: pd.DataFrame) -> None:
     X_val = test_df.drop(columns=[target_variable, time_variable_to_drop])
     y_val = test_df[target_variable]
 
-
     def objective(params):
-
         with mlflow.start_run():
             rf = RandomForestRegressor(**params)
             rf.fit(X_train, y_train)
             y_pred = rf.predict(X_val)
             rmse = mean_squared_error(y_val, y_pred, squared=False)
             mlflow.log_metric("rmse", rmse)
+            mape = mean_absolute_percentage_error(y_val, y_pred)
+            mlflow.log_metric("mape", mape)
 
-        return {'loss': rmse, 'status': STATUS_OK}
-
-
+        return {"loss": rmse, "status": STATUS_OK}
 
     rstate = np.random.default_rng(42)
     fmin(
         fn=objective,
         space=search_space,
         algo=tpe.suggest,
-        max_evals=10,
+        max_evals=5,
         trials=Trials(),
-        rstate=rstate
+        rstate=rstate,
     )
-
