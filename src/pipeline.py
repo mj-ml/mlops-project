66620009f845b5171a8134104e6e272e378e8281
Mikel <mii.jablonski@gmail.com>
2024-07-24 11:26:51 +0200

    small refactor

--- src/pipeline.py
+++ src/pipeline.py
@@ -1,11 +1,10 @@
 from src.model import register_the_best_model, fetch_model_predict, monitoring
 
 
-def training_pipeline():
+def preprocessing():
     from src.model import (
         generate_features,
         split_train_test,
-        train_model,
     )
     from src.read_data import (
         read_weather,
@@ -14,7 +13,6 @@ def training_pipeline():
         extract_load,
         create_dataframe,
     )
-
     df = read_weather()
     df_weather = extract_weather(df)
     df = read_load()
@@ -22,29 +20,19 @@ def training_pipeline():
     df_all = create_dataframe(df_weather, df_load)
     df_features = generate_features(df_all)
     df_train, df_test = split_train_test(df_features)
+    return df_train, df_test
+
+
+def training_pipeline():
+    from src.model import (
+        train_model,
+    )
+    df_train, df_test = preprocessing()
     train_model(df_train, df_test)
 
 
 def monitoring_pipeline():
-    from src.model import (
-        generate_features,
-        split_train_test,
-    )
-    from src.read_data import (
-        read_weather,
-        extract_weather,
-        read_load,
-        extract_load,
-        create_dataframe,
-    )
-
-    df = read_weather()
-    df_weather = extract_weather(df)
-    df = read_load()
-    df_load = extract_load(df)
-    df_all = create_dataframe(df_weather, df_load)
-    df_features = generate_features(df_all)
-    _, df_test = split_train_test(df_features)
+    _, df_test = preprocessing()
     mape = monitoring(df_test)
     if mape > 0.1:
         print("updating models required - rerun the pipeline")
